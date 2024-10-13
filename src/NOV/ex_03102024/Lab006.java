5df02c96dcb71648b724dff4ce5ad1929fe54411
Sainath <sainathvairagi@gmail.com>
2024-10-13 23:51:11 +0530

    printf statements

---
+++ src/NOV/ex_03102024/Lab006.java
@@ -0,0 +1,15 @@
+package NOV.ex_03102024;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        int age = 26;
+
+        System.out.printf("My age is %d", age);
+
+        System.out.println();
+
+        String name = "Sainath";
+        System.out.printf("My Name is %s", name);
+
+    }
+}
