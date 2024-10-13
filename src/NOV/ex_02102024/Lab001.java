5addac1faf5f92767457184a4fffe7bfc90832b3
Sainath <sainathvairagi@gmail.com>
2024-10-13 15:22:12 +0530

    10th Tabels

--- src/NOV/ex_02102024/Lab001.java
+++ src/NOV/ex_02102024/Lab001.java
@@ -1,4 +1,20 @@
 package NOV.ex_02102024;
 
 public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");   // Statement
+        System.out.println("Hello World!");
+
+        int emp$no = 10;
+        int emp_no = 10;
+        int _emp = 10;
+        int $sal = 20;
+        int emp = 10;
+
+
+
+
+
+    }
 }
